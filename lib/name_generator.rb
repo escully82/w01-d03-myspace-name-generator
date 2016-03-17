d9b1c11b3aa3d42288b3538abb9334849fcb5bd4
Ed Scully <escully82@gmail.com>
2016-03-17 18:47:27 -0500

    Finally done, aafter hours of frustration. It works. Now, onto building the tests.

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -2,3 +2,80 @@
 # you've seen elsewhere.
 
 require 'pry'
+
+# Removing any blank spaces within a given string
+#
+# This is working!!!
+
+def remove_blank_space(name)
+  name.gsub(/\s+/, "")
+end
+
+# Remove special characters
+#
+# This is working!!!
+#
+# Wait, no it's not! :(  Doesn't work with pry
+#
+# After some more tears, it is working again.
+
+def remove_special_characters(name)
+  name.gsub(/[^A-Za-z]/, '')
+end
+
+# Capitalizing all characters
+#
+# Intend to alternate capitalization in another method
+#
+# This is working!!!
+
+def capitalize_all(name)
+  name.upcase
+end
+
+# Alternating capitalization
+#
+# Calling out letters, relative to their numerical position and altering caps.
+#
+# This is working!!!
+
+def alternate_case(name)
+  name.gsub(/\w/).with_index{|s, i| i.even? ? s.upcase : s.downcase}
+end
+
+# Prepending the name with xX_
+#
+# This is working!!!
+
+def prepend_name(name)
+  name.prepend("xX_")
+end
+
+# Appending the name with _Xx
+#
+# This is working!!!
+
+def append_name(name)
+  name.<<("_Xx")
+end
+
+# Attempt to test results
+
+# Finally, after hours of aggravation, I am done and it works. Yay me!
+
+# When I run pry and input a name or string, consisting of spaces, numbers, or
+# special characters, the output is the expected result. For example,
+# ("!451edWar      5245@$d97  Sc u l !431y   ") returns xX_EdWaRdScUlLy_Xx
+
+def valid_name(name)
+  name_arg = name
+  name_arg = remove_blank_space(name_arg)
+  name_arg = remove_special_characters(name_arg)
+  name_arg = capitalize_all(name_arg)
+  name_arg = alternate_case(name_arg)
+  name_arg = prepend_name(name_arg)
+  name_arg = append_name(name_arg)
+end
+
+
+binding.pry
--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,5 +1,55 @@
 require_relative '../lib/name_generator.rb'
 
 # Define whatever methods you need first
+#
+# Here is where I will put my tests for prepend and include
+#
+# xX_ + "" + _Xx => true
+# Xx- + "" + -xX => false
+# 152 + "" + 84t => false
+# p3t + "" + 6pa => false
+#
+# Here is where I will put my tests for alternating caps in a string
+#
+# "MeLiSsAcIeSlIk"  => true
+# "pAuLwOoDwArD"    => false
+# "SKYELUND"        => false
+# "andreascully"    => false
+# "CASSONDRAscully" => false
+
+#
+
+
+
+def before_string
+  if xX_
+    puts ":)"
+  else
+    puts "FML!"
+  end
+end
+
+def after_string
+  if _Xx
+    puts ":("
+  else
+    puts "Why God, why?"
+  end
+end
+
+
+
+#before_string.prepend = xX_
+#  true
+#end
+
+#after_string.include = _Xx
+#  true
+#end
+
+
 
 # Now run your actual tests, making sure to do output as necessary
+def test_one
+before_string.prepend + "Hello my minions" + after_string.inlcude
+end
